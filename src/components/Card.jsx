c84c1fb76b2e2bebcb661fa311e3d237eddecf04
irfanyazid-13032000 <irfanyazid18@gmail.com>
2024-11-07 11:33:53 +0700

    membuat heart dan countdown

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,14 @@ export default function Card(){
   return (
     <>
       <div className="tampah">
+        <div className="bar">
+          <table>
+            <tr>
+              <td>❤️❤️❤</td>
+              <td>0:0:20</td>
+            </tr>
+          </table>
+        </div>
         <div className="kanji">
           <p className='tulisan_kanji'>図書館</p>
           <p className='pertanyaan'>apa arti dari kanji tersebut?</p>
